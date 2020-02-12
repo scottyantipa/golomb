a3ef88c599c84deb80ff9c9b300bd4034024bf30
Scott Antipa <scottantipa@gmail.com>
2020-02-11 16:52:50 -0800

    Add websocket

--- src/main/scala/golomb/Server.scala
+++ src/main/scala/golomb/Server.scala
@@ -1,19 +1,58 @@
 package golomb
 
-import akka.actor.ActorSystem
+import akka.{Done, NotUsed}
+import akka.actor.{Actor, ActorSystem}
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model._
+import akka.http.scaladsl.model.ws.{Message, TextMessage}
 import akka.http.scaladsl.server.Directives._
-import akka.stream.ActorMaterializer
+import akka.stream.{ActorMaterializer, OverflowStrategy, QueueOfferResult, ThrottleMode}
+import akka.stream.scaladsl._
+
+import scala.concurrent.duration._
+import scala.collection.immutable.Queue
+import scala.concurrent.Promise
 import scala.io.StdIn
 
 object Server extends App {
   override def main(args: Array[String]) {
 
-    implicit val system = ActorSystem("actor-sytem-for-server")
+    implicit val system = ActorSystem("actors")
     implicit val materializer = ActorMaterializer()
-    // needed for the future flatMap/onComplete in the end
     implicit val executionContext = system.dispatcher
+
+    // The source to broadcast (just ints for simplicity)
+    val dataSource = Source.queue[String](1000, OverflowStrategy.backpressure).throttle(1, 1 seconds, 1, ThrottleMode.Shaping).map(_.toString)
+    val p = Promise[SourceQueueWithComplete[String]]
+    val s = dataSource.mapMaterializedValue { m =>
+      p.trySuccess(m)
+      m
+    }
+
+    // get the materialized queue so we can push into it
+    p.future.map { queue =>
+      (0 to 10000).foreach { i =>
+        queue.offer(s"hello $i").map {
+          case QueueOfferResult.Enqueued => println(s"enqueued")
+          case QueueOfferResult.Dropped => println(s"dropped")
+          case QueueOfferResult.Failure(ex) => println(s"Offer failed ${ex.getMessage}")
+          case QueueOfferResult.QueueClosed => println("Source Queue closed")
+        }
+      }
+    }
+
+    // Go via BroadcastHub to allow multiple clients to connect
+    val runnableGraph: RunnableGraph[Source[String, NotUsed]] =
+      s.toMat(BroadcastHub.sink(bufferSize = 256))(Keep.right)
+
+    val producer: Source[String, NotUsed] = runnableGraph.run()
+    producer.runWith(Sink.ignore) // avoid back pressure on original flow
+
+    // TODO Ignore data sent from client
+    val wsHandler: Flow[Message, Message, NotUsed] =
+      Flow[Message]
+        .merge(producer) // Stream the data we want to the client
+        .map(l => TextMessage(l.toString))
 
     val route =
       path("ping") {
@@ -24,6 +63,17 @@ object Server extends App {
         get {
           val solution: String = GolombRuler.solve()
           complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, s"<h1>$solution</h1>"))
+        }
+      } ~ path("wstest0") {
+        get {
+          extractUpgradeToWebSocket { upgrade =>
+            val numbers = Source(1 to 10).map(i => TextMessage(i.toString))
+            complete(upgrade.handleMessagesWithSinkSource(Sink.ignore, numbers))
+          }
+        }
+      } ~ path("bhub") {
+        get {
+          handleWebSocketMessages(wsHandler)
         }
       }
 
